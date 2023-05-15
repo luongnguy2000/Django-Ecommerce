1e5aad7056583a8d922058a822552a8c5bab915a
luongnguy2000 <luongnguy2000@gmail.com>
2023-05-15 15:27:13 +0700

    full model and testing

--- ecommerce/ecommerce/tests/fixtures.py
+++ ecommerce/ecommerce/tests/fixtures.py
@@ -8,9 +8,7 @@ def create_admin_user(django_user_model):
     """
     Return admin user
     """
-    return django_user_model.objects.create_superuser(
-        "admin", "admin@admin.com", "admin"
-    )
+    return django_user_model.objects.create_superuser("admin", "a@a.com", "password")
 
 
 @pytest.fixture(scope="session")
@@ -22,3 +20,11 @@ def django_fixture_setup(django_db_setup, django_db_blocker):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_type_fixture.json")
+        call_command("loaddata", "db_brand_fixture.json")
+        call_command("loaddata", "db_product_inventory_fixture.json")
+        call_command("loaddata", "db_media_fixture.json")
+        call_command("loaddata", "db_stock_fixture.json")
+        call_command("loaddata", "db_product_attribute_fixture.json")
+        call_command("loaddata", "db_product_attribute_value_fixture.json")
+        call_command("loaddata", "db_product_attribute_values_fixture.json")
